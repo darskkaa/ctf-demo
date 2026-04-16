3c9a3d812d242bfe6b269a0cfc10a4eb5b19236c
darskkaa <johnfdark@duck.com>
2026-04-16 18:39:47 -0400

    meta tag challenge

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const keywordPartOne = "check_the";
 function checkFlag() {
   const input = document.getElementById("flagInput").value.trim();
   const result = document.getElementById("resultMessage");
-  const correctFlag = "FLAG{4dm1n_p4g3}";
+  const correctFlag = "FLAG{m3ta_d4ta}";
 
   if (input === correctFlag) {
     result.textContent = "Correct! You captured the flag.";
