264efd5bc7bf8d7d7d785a0bfdb9e605751c0d47
darskkaa <johnfdark@duck.com>
2026-04-16 18:31:45 -0400

    admin page + base64 clue

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const keywordPartOne = "check_the";
 function checkFlag() {
   const input = document.getElementById("flagInput").value.trim();
   const result = document.getElementById("resultMessage");
-  const correctFlag = "FLAG{check_the_source}";
+  const correctFlag = "FLAG{4dm1n_p4g3}";
 
   if (input === correctFlag) {
     result.textContent = "Correct! You captured the flag.";
