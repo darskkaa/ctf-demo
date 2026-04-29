9cb57334d44eeb48c838db03ced88be947b1dad1
darskkaa <johnfdark@duck.com>
2026-04-29 18:58:30 -0400

    change html

--- ctf-terminal/flag_server.py
+++ ctf-terminal/flag_server.py
@@ -4,17 +4,24 @@ TOKEN = b'open_sesame'
 FLAG  = b'FLAG{p0rt_kn0ck3r}\n'
 DENY  = b'access denied. wrong token.\n'
 
+s = socket.socket()
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+s.bind(('', 8888))
+s.listen(5)
+
 while True:
-    s = socket.socket()
-    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    s.bind(('', 8888))
-    s.listen(5)
-    c, _ = s.accept()
-    c.sendall(b'token: ')
-    data = c.recv(1024).strip()
-    if data == TOKEN:
-        c.sendall(FLAG)
-    else:
-        c.sendall(DENY)
-    c.close()
-    s.close()
+    c, addr = s.accept()
+    try:
+        c.sendall(b'token: ')
+        data = c.recv(1024).strip()
+        
+        if data == TOKEN:
+            c.sendall(FLAG)
+        else:
+            c.sendall(DENY)
+            
+    except (ConnectionResetError, BrokenPipeError):
+        pass
+        
+    finally:
+        c.close()
